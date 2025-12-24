299b70b1e68ffaf9e87bcc0f4140b85eb2831d25
Lazar Otasevic <>
2025-12-25 02:56:34 +0400

    try fix all

--- run.sh
+++ run.sh
@@ -4,12 +4,20 @@ set -e
 echo "📦 Generating project..."
 swift run xcodegen
 
+# List all iPhone simulators, pick the first one
+UDID=$(xcrun simctl list devices |
+       grep -w "iPhone" | # filter only iPhone lines
+       grep -Eo "[0-9A-Fa-f-]{36}" | # extract UUID
+       head -1)
+
 echo "🧪 Running UI tests..."
 rm -rf TestResults.xcresult
 xcodebuild test \
   -project TestApp.xcodeproj \
   -scheme TestApp \
-  -destination 'platform=iOS Simulator,name=Any iOS Device' \
+  -destination id="$UDID" \
   -enableCodeCoverage YES \
   -derivedDataPath .derivedData \
   -resultBundlePath TestResults.xcresult
+
+ 
